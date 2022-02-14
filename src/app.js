61cd1cf5c03ff868ab4b2a5e5fe20a95a8350120
Sofia <sofiaxrk@gmail.com>
2022-02-14 12:01:30 +0000

    guardando, falta css

--- src/app.js
+++ src/app.js
@@ -2,10 +2,48 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let pronoun = ["the", "our"];
+let adj = ["amazing", "best"];
+let noun = ["music", "love"];
+
+function domainGen(part1, part2, part3) {
+  for (let a = 0; a < part1.length; a++) {
+    for (let b = 0; b < part2.length; b++) {
+      for (let c = 0; c < part3.length; c++) {
+        let newName = document.createTextNode(
+          part1[a] + part2[b] + part3[c] + ".com"
+        );
+        let list = document.getElementById("domainList");
+        let element = document.createElement("div");
+        element.className = "alert alert-dark";
+        list.appendChild(element);
+        element.appendChild(newName);
+        let choose = document.createElement("input");
+        choose.className = "form-check-input";
+        choose.setAttribute("type", "radio");
+        choose.setAttribute("name", "option");
+        choose.setAttribute("value", newName.textContent);
+        element.appendChild(choose);
+      }
+    }
+  }
+  const selectors = document.querySelectorAll("input");
+  for (let i = 0; i < selectors.length; i++) {
+    selectors[i].addEventListener("input", function() {
+      if (selectors[i].checked) {
+        let chosen = selectors[i].value;
+        let title = document.querySelector("#title");
+        title.innerHTML = chosen;
+      }
+    });
+  }
+}
+document.querySelector("#buttonName").addEventListener("click", function() {
+  domainGen(pronoun, adj, noun);
+  document.querySelector("#buttonName").style.display = "none";
+});
